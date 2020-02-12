5f480c5860231d7c2e8c5b0302a84b389a9fdea6
aram <aramyazdanpanah91@gmail.com>
2020-02-12 13:46:51 +0330

    Update repc_cli.erl

--- src/repc_cli.erl
+++ src/repc_cli.erl
@@ -8,19 +8,19 @@
 %% example for single argument with tupple
 %% for call this cli => ./bin/{Appname} cli {arg1, arg2}
 %% Note: after comma we need a space
-%%      wrong => ./bin/{Appname} cli {arg1,arg2}
-%%      true => ./bin/{Appname} cli {arg1, arg2}
+%%      wrong => ./bin/{{name}} cli {arg1,arg2}
+%%      true => ./bin/{{name}} cli {arg1, arg2}
 run({Arg1,Arg2}) ->
 	io:format("in {{name}}_cli module => arg1=> ~p, arg2 => ~p, arg1_is_atom => ~p", [Arg1, Arg2, is_atom(Arg1)]);
 
 %% example for single argument
-%% for call this cli => ./bin/{Appname} cli arg
+%% for call this cli => ./bin/{{name}} cli arg
 run(Arg) ->
 	io:format("in {{name}}_cli module => arg = ~p", [Arg]).
 
 
 %% example for two argument
-%% for call this cli => ./bin/{Appname} cli arg1, arg2
+%% for call this cli => ./bin/{{name}} cli arg1, arg2
 run(Arg1, Arg2) ->
 	#{module => ?MODULE, arg1 => Arg1, arg2 => Arg2}.
 
